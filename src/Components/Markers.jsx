e5e92c4f9c5f363691acb1e32c797844a0cc9124
shivansh <soodshivansh5@gmail.com>
2024-01-01 22:22:52 +0530

    edited markers.js

--- src/Components/Markers.jsx
+++ src/Components/Markers.jsx
@@ -10,7 +10,7 @@ const Markers = () => {
             <div className="font-Signika max-w-[1300px] w-full mx-auto flex flex-col md:flex-row justify-center items-center lg:gap-20">
                 <div className="flex flex-col gap-5 text-center md:text-left">
                     <h1 className="lg:text-3xl md:text-2xl sm:text-2xl text-xl font-bold p-4 border-b border-gray-600">Discover Our Range of Flow Level Measurement Field Instruments</h1>
-                    <h1 className="lg:text-3xl md:text-2xl sm:text-2xl text-xl font-bold p-4 border-b border-gray-600">Experience the Best in Valve Solutions</h1>
+                    <h1 className="lg:text-3xl md:text-2xl sm:text-2xl text-xl font-bold p-4 border-b border-gray-600">Experience the Best in Valves and Valve Automation</h1>
                     <h1 className="lg:text-3xl md:text-2xl sm:text-2xl text-xl font-bold p-4">Discover a Wide Range of Instrument Air & Process Filters</h1>
                 </div>
                 <div className="w-full md:w-2/3 lg:w-1/2 text-center">
